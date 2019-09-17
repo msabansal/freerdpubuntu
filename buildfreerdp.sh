26cd5ba059d92d5dc6c4de91edc8b5974375ba4f
Sandeep Bansal <sabansal@microsoft.com>
2019-09-16 21:53:09 -0700

    Changes to have freerdp build inside a container

--- buildfreerdp.sh
+++ buildfreerdp.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 mkdir -p build/freerdp && cd build
-curl http://security.ubuntu.com/ubuntu/pool/universe/f/freerdp/freerdp_1.1.0~git20140921.1.440916e+dfsg1.orig.tar.xz -o freerdp_1.2.2.orig.tar.xz
+curl http://security.ubuntu.com/ubuntu/pool/universe/f/freerdp/freerdp_1.1.0~git20140921.1.440916e+dfsg1.orig.tar.xz -o freerdp_1.1.2.tar.xz
+cp ../freerdp.md5 .
+md5sum -c freerdp.md5
 cd freerdp
-tar -xJf ../freerdp_1.2.2.orig.tar.xz  --strip-components=1
+tar -xJf ../freerdp_1.1.2.tar.xz  --strip-components=1
 ln -s ../../debian
-dpkg-buildpackage -rfakeroot -us -uc
+dpkg-buildpackage -b -rfakeroot -us -uc
 cd ../ && mkdir freerdppackages && mv *.deb freerdppackages
 dpkg-scanpackages freerdppackages > freerdppackages/Packages
-
+tar -cvzf freerdppackages.tar.gz freerdppackages
